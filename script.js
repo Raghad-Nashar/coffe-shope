a1a6e985e4532835314a0576eb005e798801c889
Raghad-Nashar <158178063+Raghad-Nashar@users.noreply.github.com>
2024-06-13 14:33:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,176 @@
+const COrderoffeeMochAorder      = document.getElementById("order").elements.namedItem("Coffee-Mocha-order");
+const COrderoffeeEspressoOrder   = document.getElementById("order").elements.namedItem("Coffee-Espresso-order");
+const COrderoffeeAmericanoOrder  = document.getElementById("order").elements.namedItem("Coffee-Americano-order");
+const COrderoffeeIcedCoffeeOrder = document.getElementById("order").elements.namedItem("Coffee-Iced Coffee-order");
+const COrderoffeeCafeauLaitOrder = document.getElementById("order").elements.namedItem("Coffee-Cafe au Lait-order");
+const COrderoffeeAffogatoOrder   = document.getElementById("order").elements.namedItem("Coffee-Affogato-order");
+const COrderoffeeFlatWhiteOrder  = document.getElementById("order").elements.namedItem("Coffee-Flat White-order");
+const COrderoffeeCappuccinoOrder = document.getElementById("order").elements.namedItem("Coffee-Cappuccino-order");
+const COrderoffeeCortadoOrder    = document.getElementById("order").elements.namedItem("Coffee-Cortado-order");
+const COrderoffeeBreveOrder      = document.getElementById("order").elements.namedItem("Coffee-Breve-order");
+
+const DOrderrinkBlackberryOrder         = document.getElementById("order").elements.namedItem("Drink-Blackberry-order");
+const DOrderrinkRosemaryOrder           = document.getElementById("order").elements.namedItem("Drink-Rosemary-order");
+const DOrderrinkPeachLemonade     = document.getElementById("order").elements.namedItem("Drink-Peach Lemonade-order");
+const DOrderrinkMargarita               = document.getElementById("order").elements.namedItem("Drink-Margarita Mocktail-order");
+const DOrderrinkStrawberry             = document.getElementById("order").elements.namedItem("Drink-Strawberry Spritzers-order");
+const DOrderrinkShirley                = document.getElementById("order").elements.namedItem("Drink-Shirley Temple Drink-order");
+const DOrderrinkSparkling               = document.getElementById("order").elements.namedItem("Drink-Sparkling Plum Punch-order");
+const DOrderrinkLemonadeOrder           =document.getElementById("order").elements.namedItem("Drink-Lemonade-order");
+const DOrderrinkBasilTeaOrder           =document.getElementById("order").elements.namedItem("Drink-Basil Sweet Tea-order");
+const DOrderrinkRoyrinkOrder            = document.getElementById("order").elements.namedItem("Drink-Roy Rogers Drink-order");
+
+const  OrderCakeCookiesOrder= document.getElementById("order").elements.namedItem("Cake-Cookies-order");
+const  OrderCakeMothersDay  = document.getElementById("order").elements.namedItem("Cake-Mother’s Day Cake-order");
+const  OrderCakeBirthday    = document.getElementById("order").elements.namedItem("Cake-Birthday Cake-order");
+const  OrderCakeVanilla     = document.getElementById("order").elements.namedItem("Cake-Vanilla Cake-order");
+const  OrderCakeRedVelvet   = document.getElementById("order").elements.namedItem("Cake-Red Velvet Cake-order");
+const  OrderCakeCarrot      = document.getElementById("order").elements.namedItem("Cake-Carrot Cake-order");
+const  OrderCakeLemon       = document.getElementById("order").elements.namedItem("Cake-Lemon Cake-order");
+const  OrderCakeStrawberry  = document.getElementById("order").elements.namedItem("Cake-Strawberry Cake-order");
+const  OrderCakeCoconut     = document.getElementById("order").elements.namedItem("Cake-Coconut Cake-order");
+const  OrderCakeMarble      = document.getElementById("order").elements.namedItem("Cake-Marble Cake-order");
+
+const  OrderIceCreamGelato   = document.getElementById("order").elements.namedItem("Ice Cream-Gelato");
+const  OrderIceCreamKulfi    = document.getElementById("order").elements.namedItem("Ice Cream-Kulfi");
+const  OrderIceCreamSherbet  = document.getElementById("order").elements.namedItem("Ice Cream-Sherbet");
+const  OrderIceCreamSorbet   = document.getElementById("order").elements.namedItem("Ice Cream-Sorbet");
+const  OrderIceCreamFrozen   = document.getElementById("order").elements.namedItem("Ice Cream-Frozen Yogurt");
+const  OrderIceCreamSoftServe= document.getElementById("order").elements.namedItem("Ice Cream-Soft Serve");
+const  OrderIceCreamRolled   = document.getElementById("order").elements.namedItem("Ice Cream-Rolled Ice Cream");
+const  OrderIceCreamSnow     = document.getElementById("order").elements.namedItem("Ice Cream-Snow Cream");
+const  OrderIceCreamItalian  = document.getElementById("order").elements.namedItem("Ice Cream-Italian Ice");
+const  OrderIceCreamDondurma = document.getElementById("order").elements.namedItem("Ice Cream-Dondurma");
+
+// prising
+
+const CoffeeMochAorder      = document.getElementById("pricing").elements.namedItem("Coffee-Mocha-order");
+const CoffeeEspressoOrder   = document.getElementById("pricing").elements.namedItem("Coffee-Espresso-order");
+const CoffeeAmericanoOrder  = document.getElementById("pricing").elements.namedItem("Coffee-Americano-order");
+const CoffeeIcedCoffeeOrder = document.getElementById("pricing").elements.namedItem("Coffee-Iced Coffee-order");
+const CoffeeCafeauLaitOrder = document.getElementById("pricing").elements.namedItem("Coffee-Cafe au Lait-order");
+const CoffeeAffogatoOrder   = document.getElementById("pricing").elements.namedItem("Coffee-Affogato-order");
+const CoffeeFlatWhiteOrder  = document.getElementById("pricing").elements.namedItem("Coffee-Flat White-order");
+const CoffeeCappuccinoOrder = document.getElementById("pricing").elements.namedItem("Coffee-Cappuccino-order");
+const CoffeeCortadoOrder    = document.getElementById("pricing").elements.namedItem("Coffee-Cortado-order");
+const CoffeeBreveOrder      = document.getElementById("pricing").elements.namedItem("Coffee-Breve-order");
+
+const DrinkBlackberryOrder         = document.getElementById("pricing").elements.namedItem("Drink-Blackberry-order");
+const DrinkRosemaryOrder           = document.getElementById("pricing").elements.namedItem("Drink-Rosemary-order");
+const DrinkPeachLemonadeOrder      = document.getElementById("pricing").elements.namedItem("Drink-Peach Lemonade-order");
+const DrinkMargaritaMocktailOrder  = document.getElementById("pricing").elements.namedItem("Drink-Margarita Mocktail-order");
+const DrinkStrawberrySpritzersOrder= document.getElementById("pricing").elements.namedItem("Drink-Strawberry Spritzers-order");
+const DrinkShirleyTemplerinkkOrder = document.getElementById("pricing").elements.namedItem("Drink-Shirley Temple Drink-order");
+const DrinkSparklingPlumPunchOrder = document.getElementById("pricing").elements.namedItem("Drink-Sparkling Plum Punch-order");
+const DrinkLemonadeOrder            =document.getElementById("pricing").elements.namedItem("Drink-Lemonade-order");
+const DrinkBasilTeaOrder            =document.getElementById("pricing").elements.namedItem("Drink-Basil Sweet Tea-order");
+const DrinkRoyrinkOrder            = document.getElementById("pricing").elements.namedItem("Drink-Roy Rogers Drink-order");
+
+const  CakeCookiesOrder= document.getElementById("pricing").elements.namedItem("Cake-Cookies-order");
+const  CakeMothersDay  = document.getElementById("pricing").elements.namedItem("Cake-Mother’s Day Cake-order");
+const  CakeBirthday    = document.getElementById("pricing").elements.namedItem("Cake-Birthday Cake-order");
+const  CakeVanilla     = document.getElementById("pricing").elements.namedItem("Cake-Vanilla Cake-order");
+const  CakeRedVelvet   = document.getElementById("pricing").elements.namedItem("Cake-Red Velvet Cake-order");
+const  CakeCarrot      = document.getElementById("pricing").elements.namedItem("Cake-Carrot Cake-order");
+const  CakeLemon       = document.getElementById("pricing").elements.namedItem("Cake-Lemon Cake-order");
+const  CakeStrawberry  = document.getElementById("pricing").elements.namedItem("Cake-Strawberry Cake-order");
+const  CakeCoconut     = document.getElementById("pricing").elements.namedItem("Cake-Coconut Cake-order");
+const  CakeMarble      = document.getElementById("pricing").elements.namedItem("Cake-Marble Cake-order");
+
+const  IceCreamGelato      = document.getElementById("pricing").elements.namedItem("Ice Cream-Gelato");
+const  IceCreamKulfi       = document.getElementById("pricing").elements.namedItem("Ice Cream-Kulfi");
+const  IceCreamSherbet     = document.getElementById("pricing").elements.namedItem("Ice Cream-Sherbet");
+const  IceCreamSorbet      = document.getElementById("pricing").elements.namedItem("Ice Cream-Sorbet");
+const  IceCreamFrozenYogurt= document.getElementById("pricing").elements.namedItem("Ice Cream-Frozen Yogurt");
+const  IceCreamSoftServe   = document.getElementById("pricing").elements.namedItem("Ice Cream-Soft Serve");
+const  IceCreamRolled      = document.getElementById("pricing").elements.namedItem("Ice Cream-Rolled Ice Cream");
+const  IceCreamSnow        = document.getElementById("pricing").elements.namedItem("Ice Cream-Snow Cream");
+const  IceCreamItalian     = document.getElementById("pricing").elements.namedItem("Ice Cream-Italian Ice");
+const  IceCreamDondurma    = document.getElementById("pricing").elements.namedItem("Ice Cream-Dondurma");
+
+// get select Order
+
+function  getSelectedOrders(params){
+    return params.filter(item => item.value >0)
+}
+
+function getOrders(){
+
+    const selectedOrder=getSelectedOrders([COrderoffeeMochAorder,COrderoffeeEspressoOrder,COrderoffeeAmericanoOrder,COrderoffeeIcedCoffeeOrder
+        ,COrderoffeeCafeauLaitOrder,COrderoffeeAffogatoOrder,COrderoffeeFlatWhiteOrder,COrderoffeeCappuccinoOrder
+        ,COrderoffeeCortadoOrder,COrderoffeeBreveOrder,DOrderrinkBlackberryOrder,DOrderrinkRosemaryOrder
+        ,DOrderrinkPeachLemonade,DOrderrinkMargarita,DOrderrinkStrawberry,DOrderrinkShirley,DOrderrinkSparkling 
+        ,DOrderrinkLemonadeOrder,DOrderrinkBasilTeaOrder,DOrderrinkRoyrinkOrder ,OrderCakeCookiesOrder,OrderCakeMothersDay
+        ,OrderCakeBirthday,OrderCakeVanilla,OrderCakeRedVelvet ,OrderCakeCarrot,OrderCakeLemon ,OrderCakeStrawberry
+        ,OrderCakeCoconut,OrderCakeMarble ,OrderIceCreamGelato,OrderIceCreamKulfi,OrderIceCreamSherbet,OrderIceCreamSorbet
+        ,OrderIceCreamFrozen,OrderIceCreamSoftServe,OrderIceCreamRolled,OrderIceCreamSnow,OrderIceCreamItalian,OrderIceCreamDondurma,      
+        ]);
+        return selectedOrder;
+}
+function generateRecept(orders){
+    let receipt ='';
+    orders.map(order => {
+        // latte: 2 x 3 =6
+        const orderName = order.name.replace("-order","");
+        const orderprice= document.getElementById("pricing").elements.namedItem(orderName).value;
+        const orderValue = orderprice *  order.value;
+        total += orderValue ;
+        receipt += order.name + " : " + order.value + "x" + orderprice + " = $"+ orderValue +"." +"\n";
+    })
+     return receipt;
+}
+let total=0;
+function getTotal(){
+
+    const orders =getOrders();
+    const Receipt =generateRecept(orders);
+    console.log('Recipt',Receipt)
+
+    document.getElementById('recipt').innerText=Receipt;
+    document.getElementById('total').innerText=" Total: $"+total+".";
+    console.log('total',total)
+
+}
+
+// checklist
+
+document.addEventListener("change",()=>{
+    const orders =getOrders();
+    const Receipt =generateRecept(orders);
+    document.getElementById('Checklist').innerText=Receipt;
+
+
+})
+
+// button Rise
+let Rise=document.getElementById("Rise");
+ 
+onscroll=function(){
+    if(scrollY >= 500){
+        Rise.style.display= "block";
+    }
+    else{
+        Rise.style.display="none";
+    }
+}
+Rise.onclick=function(){
+    scroll({
+        top:0,
+        left:0,
+        behavior:"smooth",
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// generate recept function
